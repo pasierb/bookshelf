2117ea87ff3f0bcb74388f9e04f2ffa1964aba5e
pasierb <michal.pasierbski@sooftly.com>
2014-05-16 19:44:36 +0200

    basic version

--- js/app.js
+++ js/app.js
@@ -1,11 +1,51 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  // put your routes here
+App.Book = Ember.Object.extend({});
+
+App.BookCoverComponent = Ember.Component.extend({
+    click: function() {
+        this.sendAction('action', this.get('book'));
+    }
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+App.Router.map(function() {});
+
+App.IndexRoute = Ember.Route.extend({});
+
+App.IndexController = Ember.Controller.extend({
+    search: '',
+
+    matchedBooks: Ember.A(),
+
+    bookshelf: Ember.A(),
+
+    query: function() {
+        var that = this,
+            query = this.get('search');
+
+        $.getJSON(['http://turbine-staging-eu.herokuapp.com/books.json?q=', query].join(''), function(books) {
+            that.set('matchedBooks', Ember.A());
+
+            $(books).each(function(index, bookJSON) {
+                var book = App.Book.create(bookJSON);
+
+                that.get('matchedBooks').addObject(book);
+            });
+        });
+    },
+
+    onSearchQueryUpdate: function() {
+        // make sure not to kill API server
+        Ember.run.debounce(this, this.query, 200);
+    }.observes('search'),
+
+    actions: {
+        addToBookshelf: function(book) {
+            this.get('bookshelf').addObject(book);
+        },
+
+        removeFromBookshelf: function(book) {
+            this.get('bookshelf').removeObject(book);
+        }
+    }
 });
